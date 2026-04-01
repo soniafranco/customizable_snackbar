6e1146901bae2f7551ecfec122ba44a445738c8f
Sonia Franco <sonia.franco2205@gmail.com>
2026-04-01 18:01:07 +0200

    Refactor snackbar queue provider implementation and update `flutter_riverpod` dependency to version 2.6.1 for compatibility.

--- lib/src/providers/snackbar_queue_provider.g.dart
+++ lib/src/providers/snackbar_queue_provider.g.dart
@@ -6,60 +6,23 @@ part of 'snackbar_queue_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint, type=warning
-/// Provider that manages the snackbar queue.
-
-@ProviderFor(SnackbarQueue)
-const snackbarQueueProvider = SnackbarQueueProvider._();
-
-/// Provider that manages the snackbar queue.
-final class SnackbarQueueProvider
-    extends $NotifierProvider<SnackbarQueue, SnackbarQueueState> {
-  /// Provider that manages the snackbar queue.
-  const SnackbarQueueProvider._()
-      : super(
-          from: null,
-          argument: null,
-          retry: null,
-          name: r'snackbarQueueProvider',
-          isAutoDispose: true,
-          dependencies: null,
-          $allTransitiveDependencies: null,
-        );
-
-  @override
-  String debugGetCreateSourceHash() => _$snackbarQueueHash();
-
-  @$internal
-  @override
-  SnackbarQueue create() => SnackbarQueue();
-
-  /// {@macro riverpod.override_with_value}
-  Override overrideWithValue(SnackbarQueueState value) {
-    return $ProviderOverride(
-      origin: this,
-      providerOverride: $SyncValueProvider<SnackbarQueueState>(value),
-    );
-  }
-}
-
 String _$snackbarQueueHash() => r'c313ea511237e68025ebf455cadf5d5b36eeec0d';
 
 /// Provider that manages the snackbar queue.
+///
+/// Copied from [SnackbarQueue].
+@ProviderFor(SnackbarQueue)
+final snackbarQueueProvider =
+    AutoDisposeNotifierProvider<SnackbarQueue, SnackbarQueueState>.internal(
+  SnackbarQueue.new,
+  name: r'snackbarQueueProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$snackbarQueueHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
 
-abstract class _$SnackbarQueue extends $Notifier<SnackbarQueueState> {
-  SnackbarQueueState build();
-  @$mustCallSuper
-  @override
-  void runBuild() {
-    final created = build();
-    final ref = this.ref as $Ref<SnackbarQueueState, SnackbarQueueState>;
-    final element = ref.element as $ClassProviderElement<
-        AnyNotifier<SnackbarQueueState, SnackbarQueueState>,
-        SnackbarQueueState,
-        Object?,
-        Object?>;
-    element.handleValue(ref, created);
-  }
-}
+typedef _$SnackbarQueue = AutoDisposeNotifier<SnackbarQueueState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
